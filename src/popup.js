78e2dc769136cb0e6ca56400ae6f7d2c08bfadb9
Michael Incardona <michael.incardona@outlook.com>
2020-12-14 23:00:01 -0500

    Many improvements
    - Add support for multiple OSes (currently Linux + BSDs)
    - Remember last OS in local storage
    - Open manpages in current tab (may revisit later)
    - Add Ctrl+Alt+M popup shortcut
    - Fix bugs

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,28 @@
-let systems = {
-    "man7": {
-        "urlWithSection": "https://man7.org/linux/man-pages/man%QMAN_SECTION%/%QMAN_TERM%.%QMAN_SECTION%.html",
-        "urlWithoutSection": "https://www.google.com/search?q=%QMAN_TERM%&sitesearch=man7.org%2Flinux%2Fman-pages&sa=Search+online+pages"
+let oses = {
+    man7: {
+        urlWithSection: "https://man7.org/linux/man-pages/man%QMAN_SECTION_NUMBER%/%QMAN_TERM%.%QMAN_SECTION_NUMBER%%QMAN_SECTION_SUFFIX%.html",
+        urlWithoutSection: "https://www.google.com/search?q=%QMAN_TERM%&sitesearch=man7.org%2Flinux%2Fman-pages&sa=Search+online+pages"
+    },
+    freebsd: {
+        urlWithSection: "https://www.freebsd.org/cgi/man.cgi?query=%QMAN_TERM%&apropos=0&sektion=%QMAN_SECTION_NUMBER%%QMAN_SECTION_SUFFIX%&format=html",
+        urlWithoutSection: "https://www.freebsd.org/cgi/man.cgi?query=%QMAN_TERM%&apropos=0&format=html"
+    },
+    openbsd: {
+        urlWithSection: "https://man.openbsd.org/%QMAN_TERM%.%QMAN_SECTION_NUMBER%%QMAN_SECTION_SUFFIX%",
+        urlWithoutSection: "https://man.openbsd.org/%QMAN_TERM%"
+    },
+    netbsd: {
+        urlWithSection: "https://man.netbsd.org/%QMAN_TERM%.%QMAN_SECTION_NUMBER%%QMAN_SECTION_SUFFIX%",
+        urlWithoutSection: "https://man.netbsd.org/%QMAN_TERM%"
+    },
+    dragonflybsd: {
+        urlWithSection: "https://man.dragonflybsd.org/?command=%QMAN_TERM%&section=%QMAN_SECTION%%QMAN_SECTION_SUFFIX%",
+        urlWithoutSection: "https://man.dragonflybsd.org/?command=%QMAN_TERM%&section=ANY"
     }
 };
 
 let sectionNumberDescriptions = {
+    "": "",
     "1": "commands",
     "2": "system calls",
     "3": "library functions",
@@ -17,44 +34,77 @@ let sectionNumberDescriptions = {
 };
 
 let sectionSuffixes = {
+    "": "",
     "p": "POSIX"
 };
 
-let nbsp = '\xa0';
+const nbsp = '\xa0';
 
-/// sectionId = "1", "2", "3", "3p", etc.
-function getSectionDescription(sectionId) {
-    if (!sectionId) {
-        return nbsp;
+class SectionParts {
+    constructor(sectionStr) {
+        this.number = sectionStr[0] ?? "";
+        this.suffix = sectionStr[1] ?? "";
     }
-
-    let ret = "";
-
-    if (sectionNumberDescriptions.hasOwnProperty(sectionId[0])) {
-        ret = sectionNumberDescriptions[sectionId[0]];
-        if (sectionId.length == 2 && sectionSuffixes.hasOwnProperty(sectionId[1])) {
-            ret += " (" + sectionSuffixes[sectionId[1]] + ")";
-        }
-    }
-
-    return ret ? ret : nbsp;
 }
 
-let form = document.getElementById("quickman-form");
+function getSectionDescription(sectionParts) {
+    let ret = sectionNumberDescriptions[sectionParts.number ?? ""] ?? "";
+    if (!ret) {
+        return nbsp;
+    }
+    const suffix = sectionSuffixes[sectionParts.suffix ?? ""] ?? "";
+    if (!suffix) {
+        return ret;
+    }
+    ret += " (" + suffix + ")";
+    return ret;
+}
 
-form.addEventListener("submit", (e) => {
-    let term = e.target.querySelector("[name=term]").value;
-    let section = e.target.querySelector("[name=section]").value;
-    let urlKey = section ? "urlWithSection" : "urlWithoutSection";
-    let systemUrl = systems["man7"][urlKey]
-        .replaceAll("%QMAN_TERM%", term)
-        .replaceAll("%QMAN_SECTION%", section);
-    browser.tabs.create({
-        "active": true,
-        "url": systemUrl
+/* entry point */
+(async () => {
+    const form = document.getElementById("quickman-form");
+    const osSelect = form.querySelector("[name=os]");
+
+    // set default select value
+    const lastOsUsed = await browser.storage.local.get({
+        lastOsUsed: "man7"
     });
-});
+    osSelect.value = lastOsUsed.lastOsUsed;
 
-form.querySelector("[name=section]").addEventListener("input", (e) => {
-    form.querySelector(".section-description").innerText = getSectionDescription(e.target.value);
-});
+    form.addEventListener("submit", (e) => {
+        const term = e.target.querySelector("[name=term]").value;
+        const sectionStr = e.target.querySelector("[name=section]").value;
+        const sectionParts = new SectionParts(sectionStr);
+        const os = e.target.querySelector("[name=os]").value;
+
+        const urlKey = sectionStr ? "urlWithSection" : "urlWithoutSection";
+        const osUrl = oses[os][urlKey]
+            .replaceAll("%QMAN_TERM%", encodeURIComponent(term))
+            .replaceAll("%QMAN_SECTION_NUMBER%", encodeURIComponent(sectionParts.number))
+            .replaceAll("%QMAN_SECTION_SUFFIX%", encodeURIComponent(sectionParts.suffix));
+
+        // ignore the returned promise
+        browser.storage.local.set({
+            lastOsUsed: os
+        }).catch((err) => {
+            console.error("Failed to save lastOsUsed: " + err);
+        });
+
+        /*// open in new tab
+        browser.tabs.create({
+            active: true,
+            url: osUrl
+        });
+        */
+
+        browser.tabs.update({
+            active: true,
+            url: osUrl
+        });
+    });
+
+    osSelect.addEventListener("input", (e) => {
+        form.querySelector(".section-description").innerText =
+            getSectionDescription(new SectionParts(e.target.value));
+    });
+})();
